bec1ccd6d2144feddfd836df7fa0cd1ad677f353
Aly Ossama <aly.ossama@scandiweb.com>
2024-10-20 12:39:41 +0300

    Product insert request

---
+++ App/Requests/ProductStoreRequest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Requests;
+
+use App\Models\ProductFactory;
+
+class ProductStoreRequest
+{
+    public static $errors = [];
+    public static $required = 'Please, submit required data';
+    public static $invalid = 'Please, provide the data of indicated type';
+    public static $unique = 'This value already exists';
+
+    public static function validate(array $request): void
+    {
+        $rules = [
+            "sku" => ['required', 'alphaNumeric', 'unique'],
+            "name" => ['required', 'alpha'],
+            "price" => ['required', 'numeric'],
+            "type" => ['required'],
+            "size" => ['required', 'numeric'],
+            "weight" => ['required', 'numeric'],
+            "length" => ['required', 'numeric'],
+            "width" => ['required', 'numeric'],
+            "height" => ['required', 'numeric'],
+        ];
+
+        foreach ($rules as $field => $fieldRules) {
+            foreach ($fieldRules as $rule) {
+                if (isset($request[$field])) {
+                    self::validationMap($field, $rule, $request[$field]);
+                }
+            }
+        }
+    }
+
+    public static function validationMap($field, $rule, $value): void
+    {
+        $methodMap = [
+            'required' => 'validateRequired',
+            'unique' => 'validateUnique',
+            'alpha' => 'validateAlpha',
+            'numeric' => 'validateNumeric',
+            'alphaNumeric' => 'validateAlphaNumeric',
+        ];
+
+        if (array_key_exists($rule, $methodMap)) {
+            call_user_func([self::class, $methodMap[$rule]], $value, $field);
+        }
+    }
+
+    public static function validateRequired($value, $field): void
+    {
+        if (isset($field) && empty($value)) {
+            self::$errors[$field]['required'] = self::$required;
+        }
+    }
+
+    public static function validateAlpha($value, $field): void
+    {
+        if (!preg_match('/^[a-zA-Z]+( [a-zA-Z]+)*$/', $value)) {
+            self::$errors[$field]['alpha'] = self::$invalid;
+        }
+    }
+
+    public static function validateNumeric($value, $field): void
+    {
+        if (!is_numeric($value) || $value < 0) {
+            self::$errors[$field]['numeric'] = self::$invalid;
+        }
+    }
+
+    public static function validateAlphaNumeric($value, $field): void
+    {
+        if (!preg_match('/^[a-zA-Z0-9]+$/', $value)) {
+            self::$errors[$field]['alphaNumeric'] = self::$invalid;
+        }
+    }
+
+    public static function validateUnique($value, $field): void
+    {
+        $product = new ProductFactory();
+        if ($product->exists($value)) {
+            self::$errors[$field]['exists'] = self::$unique;
+        }
+    }
+
+    public static function getErrors(): array
+    {
+        return self::$errors;
+    }
+}
